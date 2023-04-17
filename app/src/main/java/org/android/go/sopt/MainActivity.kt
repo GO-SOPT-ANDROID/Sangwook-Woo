7c0e0eceb5f8292f48d42aec1faf623840866725
Sangwook123 <operawook@catholic.ac.kr>
2023-04-18 00:21:57 +0900

    필수과제
    1. fragment 만들기
    2. homefragment에 recycler view 만들기
    3. homefragment에 adapter사용해서 recycler view에서 보여질 데이터 받아오기
    4. bottom navigation을 fragment 들과 연결

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -2,10 +2,45 @@ package org.android.go.sopt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import org.android.go.sopt.Gallery.GalleryFragment
+import org.android.go.sopt.Home.HomeFragment
+import org.android.go.sopt.Search.SearchFragment
+import org.android.go.sopt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.cv_main)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.cv_main, HomeFragment.newInstance()).commit()
+        }
+
+        binding.bnvMain.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.menu_home -> {
+                    changeFragment(HomeFragment())
+                }
+                R.id.menu_gallery->{
+                    changeFragment(GalleryFragment())
+                }
+                R.id.menu_search -> {
+                    changeFragment(SearchFragment())
+                }
+            }
+            true
+        }
+    }
+    private fun changeFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.cv_main,fragment)
+            .commit()
     }
 }
