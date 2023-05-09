c8f2c290f41c25cb59006525adf5bc2326bd4f2b
Sangwook123 <operawook@catholic.ac.kr>
2023-05-09 10:33:57 +0900

    로그인 xml 구성
    loginactivity의 뷰 제작

---
+++ app/src/main/java/org/android/go/sopt/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package org.android.go.sopt.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.android.go.sopt.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
