a77987a7292ac12a6988a22f8b515fd2997a8e13
Sangwook123 <operawook@catholic.ac.kr>
2023-05-10 17:10:12 +0900

    login Acticity, Signup Activity 구현

--- app/src/main/java/org/android/go/sopt/login/LoginActivity.kt
+++ app/src/main/java/org/android/go/sopt/login/LoginActivity.kt
@@ -1,15 +1,69 @@
 package org.android.go.sopt.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
+import org.android.go.sopt.MainActivity
 import org.android.go.sopt.databinding.ActivityLoginBinding
+import org.android.go.sopt.signup.SignupActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var name = intent.getStringExtra("name")
+        var special = intent.getStringExtra("special")
+        var identity = intent.getStringExtra("identity")
+        var password = intent.getStringExtra("password")
+
+        resultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){ result ->
+            if(result.resultCode == RESULT_OK){
+                val data: Intent? = result.data
+                name = data?.getStringExtra("name")
+                special = data?.getStringExtra("special")
+                identity = data?.getStringExtra("identity")
+                password = data?.getStringExtra("password")
+                Snackbar.make(
+                    binding.root,
+                    "회원가입을 완료했습니다.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        binding.btnLoginpageSignup.setOnClickListener {
+            val intent = Intent(this,SignupActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+
+        binding.btnLoginpageLogin.setOnClickListener {
+            if(binding.etLoginpageLogin.text.toString().equals(identity)&&binding.etLoginpagePassword.text.toString().equals(password)){
+                val mintent = Intent(this, MainActivity::class.java)
+                mintent.putExtra("name",name)
+                mintent.putExtra("special",special)
+                startActivity(mintent)
+            }else if(binding.etLoginpageLogin.text.toString().equals(identity)&&!(binding.etLoginpagePassword.text.toString().equals(password))){
+                Snackbar.make(
+                    binding.root,
+                    "비밀번호가 틀렸습니다.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }else{
+                Snackbar.make(
+                    binding.root,
+                    "아이디가 틀렸습니다.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
--- app/src/main/java/org/android/go/sopt/signup/SignupActivity.kt
+++ app/src/main/java/org/android/go/sopt/signup/SignupActivity.kt
@@ -1,14 +1,51 @@
 package org.android.go.sopt.signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import org.android.go.sopt.databinding.ActivitySignupBinding
+import org.android.go.sopt.login.LoginActivity
 
 class SignupActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySignupBinding
+    private lateinit var viewBinding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignupBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        viewBinding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.btnSignuppageComplete.setOnClickListener {
+            val name = viewBinding.etSignuppagePutname.text.toString()
+            val special = viewBinding.etSignuppagePutspecial.text.toString()
+            val identity = viewBinding.etSignuppagePutid.text.toString()
+            val password = viewBinding.etSignuppagePutpassword.text.toString()
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            if(identity.length < 6 || identity.length>10){
+                Snackbar.make(
+                    viewBinding.root,
+                    "아이디는 6자~10자 사이로 입력해주세요.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }else if(password.length < 8 || password.length > 12){
+                Snackbar.make(
+                    viewBinding.root,
+                    "비밀번호는 8자~12자 사이로 입력해주세요.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }else if(identity.equals("") || special.equals("")){
+                Snackbar.make(
+                    viewBinding.root,
+                    "이름과 특기를 입력해주세요.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }else{
+                intent.putExtra("name",name)
+                intent.putExtra("special",special)
+                intent.putExtra("identity",identity)
+                intent.putExtra("password", password)
+                setResult(RESULT_OK,intent)
+                finish()
+            }
+        }
     }
 }
